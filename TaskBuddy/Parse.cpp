df206d2a792299d6584ddea3f0c558c39699b503
lim_yi_min <lim_yi_min@hotmail.com>
2013-10-09 09:54:34 +0800

    Renamed Passer to Parse

---
+++ TaskBuddy/Parse.cpp
@@ -0,0 +1,48 @@
+#include "Parse.h" 
+
+const std::string Parse::KEYWORD_ADD = "add";
+const std::string Parse::KEYWORD_LOCATION = "at";
+const std::string Parse::KEYWORD_STARTING = "from";
+const std::string Parse::KEYWORD_ENDING = "to";
+const std::string Parse::KEYWORD_DEADLINE = "by";
+
+//takes in a string creates a Task object 
+Task Parse::generateTask(std::string){
+}
+
+//splits input string into various components
+void Parse::splitTaskString(std::string){
+}
+
+void Parse::setTask(std::string){
+}
+
+void Parse::setAction(std::string){
+}
+
+void Parse::setLocation(std::string){
+}
+
+void Parse::setStartingDate(std::string){
+}
+
+void Parse::setStartingTime(std::string){
+}
+
+void Parse::setEndingDate(std::string){
+}
+
+void Parse::setEndingTime(std::string){
+}
+
+void Parse::setDeadlineDate(std::string){
+}
+
+void Parse::setDeadlineTime(std::string){
+}
+
+Task::Date Parse::convertToDate(std::string){
+}
+
+int Parse::convertToTime(std::string){
+}
