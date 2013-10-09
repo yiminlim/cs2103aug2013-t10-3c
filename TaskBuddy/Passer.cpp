3c7d641b807ed6444a28c8e72380322c41c4106d
lim_yi_min <lim_yi_min@hotmail.com>
2013-10-09 09:37:17 +0800

    Added Passer class with initial methods & static keywords

---
+++ TaskBuddy/Passer.cpp
@@ -0,0 +1,48 @@
+#include "Passer.h" 
+
+const std::string Passer::KEYWORD_ADD = "add";
+const std::string Passer::KEYWORD_LOCATION = "at";
+const std::string Passer::KEYWORD_STARTING = "from";
+const std::string Passer::KEYWORD_ENDING = "to";
+const std::string Passer::KEYWORD_DEADLINE = "by";
+
+//takes in a string creates a Task object 
+Task Passer::generateTask(std::string){
+}
+
+//splits input string into various components
+void Passer::splitTaskString(std::string){
+}
+
+void Passer::setTask(std::string){
+}
+
+void Passer::setAction(std::string){
+}
+
+void Passer::setLocation(std::string){
+}
+
+void Passer::setStartingDate(std::string){
+}
+
+void Passer::setStartingTime(std::string){
+}
+
+void Passer::setEndingDate(std::string){
+}
+
+void Passer::setEndingTime(std::string){
+}
+
+void Passer::setDeadlineDate(std::string){
+}
+
+void Passer::setDeadlineTime(std::string){
+}
+
+Task::Date Passer::convertToDate(std::string){
+}
+
+int Passer::convertToTime(std::string){
+}
