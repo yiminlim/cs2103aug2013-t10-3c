520e2a5d91bbd421c70caa8d31e3186da5ac7f7f
Maung Thi Ha <Maung Thi Ha@localhost>
2013-10-20 03:18:49 +0800

    Added in Date Calendar and Edit

--- TaskBuddy/Task.cpp
+++ TaskBuddy/Task.cpp
@@ -5,8 +5,8 @@ Task::Task(){
 }
 
 //constructor
-Task::Task(std::string task, std::string action, std::string location, Date startingDate, int startingTime, Date endingDate, int endingTime, Date deadlineDate, int deadlineTime){
-	_task = task;
+Task::Task(std::string action, std::string location, Date startingDate, int startingTime, Date endingDate, int endingTime, Date deadlineDate, int deadlineTime){
+	_task = formatTask(action,location,startingDate,startingTime,endingDate,endingTime,deadlineDate,deadlineTime);
 	_action = action;
 	_location = location;
 	_startingDate = startingDate;
@@ -68,3 +68,47 @@ int Task::getDeadlineTime(){
 	return temp;
 }
 */
+
+std::string Task::formatTask(std::string action, std::string location, Date startingDate, int startingTime, Date endingDate, int endingTime, Date deadlineDate, int deadlineTime, bool isDeadLineType) {
+	std::ostringstream output;
+	if (isDeadLineType) {
+		assert (deadlineTime >= 0 && deadlineTime <= 2359);
+		output << "by " << deadlineDate._day << "/" << deadlineDate._month << "/" << deadlineDate._year;
+		output << " " << formatTimeOutputString(deadlineTime) << " hrs";
+		output << ": " << action;
+		if (location.size() > 0) {
+			output << " at " << location;
+		}
+	}
+	else {
+		output << startingDate._day << "/" << startingDate._month << "/" << startingDate._year;
+		output << " " << formatTimeOutputString(startingTime) << " hrs";
+		if (endingDate._day && endingDate._month && endingDate._year) {
+			output << " - " << endingDate._day << "/" << endingDate._month << "/" << endingDate._year;
+			output << " " << formatTimeOutputString(endingTime) << " hrs";
+		}
+		output << ": " << action;
+		if (location.size() > 0) {
+		output << " at " << location;
+		}
+	}
+
+	return output.str();
+}
+
+std::string Task::formatTimeOutputString(int time){
+	std::ostringstream timeString;
+
+	if (time < 1000) {
+		timeString << "0";
+	}
+	if (time < 100) {
+		timeString << "0";
+	}
+	if (time < 10) {
+		timeString << "0";
+	}
+	timeString << time;
+
+	return timeString.str();
+}
