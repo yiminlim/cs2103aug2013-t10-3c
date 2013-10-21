200500e29fe04f853eb32bee8dd615d63c9b8845
Maung Thi Ha <Maung Thi Ha@localhost>
2013-10-21 19:20:13 +0800

    changed logic and date

---
+++ TaskBuddy/Task.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <assert.h>
+#include "Date.h"
+
+
+class Task{
+private:
+	std::string _task;	//whole line in proper output format 
+	std::string _action; 
+	std::string _location;
+	Date _startingDate;
+	int	_startingTime;
+	Date _endingDate;
+	int _endingTime;
+	Date _deadlineDate;
+	int	_deadlineTime;
+	//char _status;
+	//std::vector <std::string> _block;
+
+public:
+	//empty constructor
+	Task();
+
+	//constructor
+	Task(std::string, std::string, Date, int, Date, int, Date, int);
+
+	//returns formatted task output string
+	std::string getTask();
+
+	//returns task action
+	std::string getAction();
+
+	//returns task location
+	std::string getLocation();
+
+	//returns task starting date
+	Date getStartingDate();
+
+	//returns task starting time
+	int getStartingTime();
+
+	//returns tsak ending date
+	Date getEndingDate();
+
+	//returns task ending time
+	int getEndingTime();
+
+	//returns task deadline date
+	Date getDeadlineDate();
+
+	//returns task deadline time
+	int getDeadlineTime();
+
+	//returns task status
+	char getStatus();
+
+	//returns task string in the desired format
+	std::string formatTask(std::string, std::string, Date, int, Date, int, Date, int, bool);
+
+	std::string formatTimeOutputString(int time);
+};
