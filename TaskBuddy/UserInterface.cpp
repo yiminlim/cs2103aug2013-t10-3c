6970c9ee81547e35b8c4ad4b47e983f4d08b32e3
Weiyuan <twy91dan@gmail.com>
2013-10-28 03:44:55 +0800

    1. tidy up comments in UserInterface class 2. add in clear function(clear the command prompt screen) 3. add in task to be done by today at initialisation of program 4. tidy up success message and fail message 5. change welcome message displayed

--- TaskBuddy/UserInterface.cpp
+++ TaskBuddy/UserInterface.cpp
@@ -4,35 +4,43 @@ const std::string UserInterface::COMMAND_ADD = "add";
 const std::string UserInterface::COMMAND_DELETE = "delete";
 const std::string UserInterface::COMMAND_SEARCH = "search";
 const std::string UserInterface::COMMAND_EDIT = "edit";
-const std::string UserInterface::COMMAND_SAVE = "save";
+const std::string UserInterface::COMMAND_UNDO = "undo";
+const std::string UserInterface::COMMAND_CLEAR = "clear";
 const std::string UserInterface::COMMAND_EXIT = "exit";
-//const std::string COMMAND_DISPLAY = "display";
 
-const std::string UserInterface::MESSAGE_WELCOME = "Welcome to Task Buddy!";
+const std::string UserInterface::KEYWORD_TODAY = "today";
+
+const std::string UserInterface::MESSAGE_TODAY_TASK = "Task(s) due by TODAY!";
+const std::string UserInterface::MESSAGE_NO_TASK_TODAY = "No task due today!";
+const std::string UserInterface::MESSAGE_COMMAND = "command: ";
 const std::string UserInterface::MESSAGE_ADD = "Task is added";
 const std::string UserInterface::MESSAGE_DELETE = "Task is deleted";
 const std::string UserInterface::MESSAGE_EDIT = "Task is edited";
-const std::string UserInterface::MESSAGE_COMMAND = "command: ";
+const std::string UserInterface::MESSAGE_UNDO = "Previous command is undone";
+const std::string UserInterface::MESSAGE_INVALID_ADD = "Task cannot be added";
+const std::string UserInterface::MESSAGE_INVALID_SEARCH = "No task is found";
+const std::string UserInterface::MESSAGE_INVALID_DELETE = "Task cannot be deleted";
+const std::string UserInterface::MESSAGE_INVALID_EDIT = "Task cannot be edited";
+const std::string UserInterface::MESSAGE_INVALID_UNDO = "Previous command cannot be undone";
 const std::string UserInterface::MESSAGE_INVALID_COMMAND = "Invalid command";
 const std::string UserInterface::MESSAGE_EXIT = "Thank you for using Task Buddy!";
 
+//User interface constructor
 UserInterface::UserInterface(){
 
 }
 
-//Starts the UserInterface from main.
-//To initialise the taskLinkedList inside tbLogic by sending an initialising command to tbLogic.
+//To initialise the program by calling for TaskLogic initialisation function
 void UserInterface::initUI(){
 	tbLogic.initLogic();
-	std::cout << MESSAGE_WELCOME << std::endl;
+	displayWelcomeMessage();
+	displayTodayTask();
 	return;
 }
 	
-//To read in different commands and the details of task if given using readTask.
-//Given different commands, it calls out the corresponding function in tbLogic.
+//To read in different commands by user and call for the respective TaskLogic function
 void UserInterface::commandUI(){
 	std::string command;
-	std::string task;
 	char space;
 	bool contProgram = true;
 	std::vector<std::string> display;
@@ -45,10 +53,10 @@ void UserInterface::commandUI(){
 		if (command == COMMAND_ADD){		
 			if (tbLogic.add(readTask(command))){
 				tbLogic.save();
-				displaySuccessfulMessage(COMMAND_ADD);				
+				displaySuccessfulMessage(command);				
 			}
 			else{
-				displayFailMessage();
+				displayFailMessage(command);
 			}
 		}
 		else if (command == COMMAND_SEARCH){
@@ -59,7 +67,7 @@ void UserInterface::commandUI(){
 				}
 			}
 			else{
-				displayFailMessage();
+				displayFailMessage(command);
 			}
 		}
 		else if (command == COMMAND_DELETE){
@@ -67,38 +75,55 @@ void UserInterface::commandUI(){
 			std::cin >> option;
 			if (tbLogic.del(display[option-1])){
 				tbLogic.save();
-				displaySuccessfulMessage(COMMAND_DELETE);			
+				displaySuccessfulMessage(command);			
 			}
 			else{
-				displayFailMessage();
+				displayFailMessage(command);
 			}
 		}
 		else if (command == COMMAND_EDIT){
 			int option;
 			std::string editString;
 			std::cin >> option;
-			std::getline(std::cin, editString);
-			editString = "add " + editString; //adding 'add ' to the start 
+			editString = "add " + readTask(COMMAND_EDIT); //adding 'add ' to the start 
 			if (tbLogic.edit(display[option-1], editString)){
 				tbLogic.save();
-				displaySuccessfulMessage(COMMAND_EDIT);
+				displaySuccessfulMessage(command);
 			}
 			else{
-				displayFailMessage();
+				displayFailMessage(command);
 			}
 		}
-		else if(command == COMMAND_EXIT){
+		else if (command == COMMAND_EXIT){
 			tbLogic.exitLogic();
-			displaySuccessfulMessage(COMMAND_EXIT);
+			displaySuccessfulMessage(command);
 			contProgram = false;
 		}
+		/*
+		else if (command == COMMAND_UNDO){
+			if (tbLogic.undo()){
+				displaySuccessfulMessage(command);
+			}
+			else{
+				displayFailMessage(command);
+			}
+		}
+		*/
+		else if (command == COMMAND_CLEAR){
+			system("CLS");
+		}
 		else{
-			displayFailMessage();
-		}		
+			displayFailMessage("");
+		}
+
+		if (command != COMMAND_CLEAR){
+			std::cout << std::endl;
+		}
+
 	} while(contProgram);
 }
 	
-//To read in the task details
+//To read in the details of task (excluding command)
 std::string UserInterface::readTask(const std::string command){
 	std::string task;
 	std::getline(std::cin, task);
@@ -110,16 +135,51 @@ std::string UserInterface::readTask(const std::string command){
 			do{
 				std::getline(std::cin, block);
 				if (block != "end"){
-					task = task + " " + block; //add space
+					task = task + " " + block; 
 				}
 			} while(block != "end");
 		}	
 	}
 	return task;
 }
+
+//To display welcome message after the program is initialised
+void UserInterface::displayWelcomeMessage(){
+	std::cout << "===========================================================================" << std::endl
+			  << "=                                                                         =" << std::endl
+		      << "=           ==========       ==         =======    ==    ==               =" << std::endl
+			  << "=               ==         ==  ==       ==         ==  ==                 =" << std::endl
+			  << "=               ==        ========      =======    ====                   =" << std::endl
+			  << "=               ==       ==      ==          ==    ==  ==                 =" << std::endl
+			  << "=               ==      ==        ==    =======    ==    ==               =" << std::endl
+			  << "=                                                                         =" << std::endl
+			  << "=        ======     ==     ==     =====       =====      ==    ==         =" << std::endl
+			  << "=        ==   ==    ==     ==     ==   ==     ==   ==     ==  ==          =" << std::endl
+			  << "=        ======     ==     ==     ==    ==    ==    ==     ====           =" << std::endl
+			  << "=        ==   ==    ==     ==     ==   ==     ==   ==       ==            =" << std::endl
+			  << "=        ======       =====       =====       =====         ==            =" << std::endl
+			  << "=                                                                         =" << std::endl
+			  << "===========================================================================" << std::endl;
+}
+
+//To display the tasks to be done for that day
+void UserInterface::displayTodayTask(){
+	std::vector<std::string> todayTask;
+
+	if (tbLogic.generalSearch(KEYWORD_TODAY, todayTask)){
+		std::cout << MESSAGE_TODAY_TASK << std::endl;
+		for (unsigned int i = 0; i < todayTask.size(); i++){
+			std::cout << i+1 << ". " << todayTask[i] << std::endl;
+		}
+	}
+	else{
+		displayFailMessage(KEYWORD_TODAY);
+	}
+	std::cout << std::endl;
+}
 	
-//To display only successful messages to User
-void UserInterface::displaySuccessfulMessage(std::string command){
+//To display messages when commands are executed successfully
+void UserInterface::displaySuccessfulMessage(const std::string command){
 	if (command == COMMAND_ADD){
 		std::cout << MESSAGE_ADD;
 	}
@@ -129,18 +189,36 @@ void UserInterface::displaySuccessfulMessage(std::string command){
 	else if (command == COMMAND_EDIT){
 		std::cout << MESSAGE_EDIT;
 	}
+	else if (command == COMMAND_UNDO){
+		std::cout << MESSAGE_UNDO;
+	}
 	else if (command == COMMAND_EXIT){
 		std::cout << MESSAGE_EXIT;
 	}
 	std::cout << std::endl;
 }
 
-//To display only fail messages to User
-void UserInterface::displayFailMessage(){
-	std::cout << MESSAGE_INVALID_COMMAND << std::endl;
+//To display messages when commands fail to execute successfully
+void UserInterface::displayFailMessage(const std::string command){
+	if (command == KEYWORD_TODAY){
+		std::cout << MESSAGE_NO_TASK_TODAY << std::endl;
+	}
+	else if (command == COMMAND_ADD){
+		std::cout << MESSAGE_INVALID_ADD << std::endl;
+	}
+	else if (command == COMMAND_SEARCH){
+		std::cout << MESSAGE_INVALID_SEARCH << std::endl;
+	}
+	else if (command == COMMAND_DELETE){
+		std::cout << MESSAGE_INVALID_DELETE << std::endl;
+	}
+	else if (command == COMMAND_EDIT){
+		std::cout << MESSAGE_INVALID_EDIT << std::endl;
+	}
+	else if (command == COMMAND_UNDO){
+		std::cout << MESSAGE_INVALID_UNDO << std::endl;
+	}
+	else{
+		std::cout << MESSAGE_INVALID_COMMAND << std::endl;
+	}
 }
-
-//To display main menu
-//void UserInterface::displayMainMenu(){
-
-//}
