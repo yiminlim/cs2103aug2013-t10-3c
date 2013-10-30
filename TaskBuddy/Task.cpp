08a2591ded311af941f524633643c9945f6d7e2f
lim_yi_min <lim_yi_min@hotmail.com>
2013-10-30 13:55:21 +0800

    Modified formatting of task string to a more general case

--- TaskBuddy/Task.cpp
+++ TaskBuddy/Task.cpp
@@ -179,9 +179,9 @@ std::string Task::formatTask() {
 			output << " " << formatTimeOutputString(_startingTime) << " hrs";
 		}
 		if (!isEmptyDate(_endingDate) || !isEmptyTime(_endingTime)) {
-			output << " - ";
+			output << " -";
 			if (!isEmptyDate(_endingDate)) {
-				output << _endingDate._day << "/" << _endingDate._month << "/" << _endingDate._year;
+				output << " " << _endingDate._day << "/" << _endingDate._month << "/" << _endingDate._year;
 			}
 			if (!isEmptyTime(_endingTime)) {
 				output << " " << formatTimeOutputString(_endingTime) << " hrs";
