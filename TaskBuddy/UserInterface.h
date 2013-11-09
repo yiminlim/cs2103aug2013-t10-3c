be72c6551d20622e12329968ab25403ae023bc84
Weiyuan <twy91dan@gmail.com>
2013-11-10 04:42:48 +0800

    1. remove fail message for search, add, addblock 2. include dateVector for search

--- TaskBuddy/UserInterface.h
+++ TaskBuddy/UserInterface.h
@@ -55,21 +55,17 @@ private:
 	static const std::string MESSAGE_CLEAROVERDUE;
 	static const std::string MESSAGE_EXIT;
 
-	static const std::string ERROR_NO_TASK_TODAY;
 	static const std::string ERROR_INVALID_COMMAND;	
 	static const std::string ERROR_SEARCH_BEFORE;
 	static const std::string ERROR_OUT_OF_VECTOR_RANGE;
 	static const std::string ERROR_UNDO_INITIALISE;
 
-	static const std::string MESSAGE_INVALID_ADD;
-	static const std::string MESSAGE_INVALID_SEARCH;
 	static const std::string MESSAGE_INVALID_DELETE;
 	static const std::string MESSAGE_INVALID_EDIT;
 	static const std::string MESSAGE_INVALID_MARKDONE;
 	static const std::string MESSAGE_INVALID_DONE;
 	static const std::string MESSAGE_INVALID_OVERDUE;
 	static const std::string MESSAGE_INVALID_UNDO;
-	static const std::string MESSAGE_INVALID_ADDBLOCK;
 	static const std::string MESSAGE_INVALID_EDITALL;
 	static const std::string MESSAGE_INVALID_DELETEBLOCK;
 	static const std::string MESSAGE_INVALID_FINALISE;
