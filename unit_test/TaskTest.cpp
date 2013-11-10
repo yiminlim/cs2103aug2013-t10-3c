b7da1266c47c85d5ebe8671147e6d6c0d8e0b713
lim_yi_min <lim_yi_min@hotmail.com>
2013-11-11 01:55:52 +0800

    Test for checking of task types and for editing block

--- unit_test/TaskTest.cpp
+++ unit_test/TaskTest.cpp
@@ -111,3 +111,72 @@ TEST(TaskTest, FormatDeadlineTaskWithTime){
 	std::string expectedTaskString = "by 11/11/2013 2359 hrs : action";
 	EXPECT_EQ(expectedTaskString, task.formatTaskOutputString());
 }
+
+TEST(TaskTest, CheckingDeadlineTaskType){
+	std::string action = "action";
+	std::string location = "";
+	Date startingDate;
+	int startingTime = -1;
+	Date endingDate;
+	int endingTime = -1;
+	Date deadlineDate(11,11,2013);
+	int deadlineTime = 2359;
+	bool block = false; 
+
+	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
+	EXPECT_EQ(true, task.isDeadlineType());
+	EXPECT_EQ(false, task.isActivityType());
+	EXPECT_EQ(false, task.isFloatingType());
+}
+
+TEST(TaskTest, CheckingActivityTaskType){
+	std::string action = "action";
+	std::string location = "";
+	Date startingDate(11,11,2013);
+	int startingTime = -1;
+	Date endingDate(14,11,2013);
+	int endingTime = -1;
+	Date deadlineDate;
+	int deadlineTime = -1;
+	bool block = false; 
+
+	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
+	EXPECT_EQ(false, task.isDeadlineType());
+	EXPECT_EQ(true, task.isActivityType());
+	EXPECT_EQ(false, task.isFloatingType());
+}
+
+TEST(TaskTest, CheckingFloatingTaskType){
+	std::string action = "action";
+	std::string location = "location";
+	Date startingDate;
+	int startingTime = -1;
+	Date endingDate;
+	int endingTime = -1;
+	Date deadlineDate;
+	int deadlineTime = -1;
+	bool block = false; 
+
+	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
+	EXPECT_EQ(false, task.isDeadlineType());
+	EXPECT_EQ(false, task.isActivityType());
+	EXPECT_EQ(true, task.isFloatingType());
+}
+
+TEST(TaskTest, EditingBlock){
+	std::string action = "action";
+	std::string location = "";
+	Date startingDate;
+	int startingTime = -1;
+	Date endingDate;
+	int endingTime = -1;
+	Date deadlineDate(11,11,2013);
+	int deadlineTime = 2359;
+	bool block = false; 
+
+	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
+	task.setBlock(true);
+	EXPECT_EQ(true, task.getBlock());
+	std::string newExpectedOutputString = "by 11/11/2013 2359 hrs : action (blockoff)";
+	EXPECT_EQ(newExpectedOutputString, task.formatTaskOutputString());
+}
