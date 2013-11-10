b5f74abccc8d499be6c10e8a0795854b5d39bd18
lim_yi_min <lim_yi_min@hotmail.com>
2013-11-11 01:34:31 +0800

    Implemented testing for Task

--- unit_test/TaskTest.cpp
+++ unit_test/TaskTest.cpp
@@ -1,7 +1,54 @@
 #include "TaskTest.h"
 
+TEST(TaskTest, FormatDateWithSingleDigitDay){
+	Task task;
+	Date date(1,10,2013);
+	std::string expectedDateString = "01/10/2013";
+	EXPECT_EQ(expectedDateString, task.formatDateOutputString(date));
+}
+TEST(TaskTest, FormatDateWithSingleDigitMonth){
+	Task task;
+	Date date(12,1,2013);
+	std::string expectedDateString = "12/01/2013";
+	EXPECT_EQ(expectedDateString, task.formatDateOutputString(date));
+}
 
-TEST_F(TaskTest, FormatFloatingTaskStringWithLocation) {
+TEST(TaskTest, FormatDateWithSingleDigitDayAndMonth){
+	Task task;
+	Date date(1,1,2013);
+	std::string expectedDateString = "01/01/2013";
+	EXPECT_EQ(expectedDateString, task.formatDateOutputString(date));
+}
+
+TEST(TaskTest, FormatTimeWithSingleDigitValue){
+	Task task;
+	int time = 5;
+	std::string expectedTimeString = "0005";
+	EXPECT_EQ(expectedTimeString, task.formatTimeOutputString(time));
+}
+
+TEST(TaskTest, FormatTimeWithDoubleDigitValue){
+	Task task;
+	int time = 30;
+	std::string expectedTimeString = "0030";
+	EXPECT_EQ(expectedTimeString, task.formatTimeOutputString(time));
+}
+
+TEST(TaskTest, FormatTimeWithTripleDigitValue){
+	Task task;
+	int time = 845;
+	std::string expectedTimeString = "0845";
+	EXPECT_EQ(expectedTimeString, task.formatTimeOutputString(time));
+}
+
+TEST(TaskTest, FormatTimeWithFourDigitValue){
+	Task task;
+	int time = 1230;
+	std::string expectedTimeString = "1230";
+	EXPECT_EQ(expectedTimeString, task.formatTimeOutputString(time));
+}
+
+TEST(TaskTest, FormatFloatingTaskStringWithLocation){
 	std::string action = "action";
 	std::string location = "location";
 	Date startingDate;
@@ -10,11 +57,57 @@ TEST_F(TaskTest, FormatFloatingTaskStringWithLocation) {
 	int endingTime = -1;
 	Date deadlineDate;
 	int deadlineTime = -1;
-	bool block = true;
+	bool block = false;
 	
 	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
 	std::string expectedTaskString = "   action at location";
-
 	EXPECT_EQ(expectedTaskString, task.formatTaskOutputString());
 }
 
+TEST(TaskTest, FormatActivityTaskWithEndWithTime){
+	std::string action = "action";
+	std::string location = "";
+	Date startingDate(11,11,2013);
+	int startingTime = 800;
+	Date endingDate(11,11,2013);
+	int endingTime = 900;
+	Date deadlineDate;
+	int deadlineTime = -1;
+	bool block = false; 
+
+	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
+	std::string expectedTaskString = "   11/11/2013 0800 hrs - 11/11/2013 0900 hrs : action";
+	EXPECT_EQ(expectedTaskString, task.formatTaskOutputString());
+}
+
+TEST(TaskTest, FormatBlockedActivityTaskWithLocation){
+	std::string action = "action";
+	std::string location = "location";
+	Date startingDate(11,11,2013);
+	int startingTime = -1;
+	Date endingDate;
+	int endingTime = -1;
+	Date deadlineDate;
+	int deadlineTime = -1;
+	bool block = true; 
+
+	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
+	std::string expectedTaskString = "   11/11/2013 : action at location (blockoff)";
+	EXPECT_EQ(expectedTaskString, task.formatTaskOutputString());
+}
+
+TEST(TaskTest, FormatDeadlineTaskWithTime){
+	std::string action = "action";
+	std::string location = "";
+	Date startingDate;
+	int startingTime = -1;
+	Date endingDate;
+	int endingTime = -1;
+	Date deadlineDate(11,11,2013);
+	int deadlineTime = 2359;
+	bool block = false; 
+
+	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
+	std::string expectedTaskString = "by 11/11/2013 2359 hrs : action";
+	EXPECT_EQ(expectedTaskString, task.formatTaskOutputString());
+}
