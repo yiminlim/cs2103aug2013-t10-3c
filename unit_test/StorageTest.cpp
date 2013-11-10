16ad0b74bb31aae74e381c64a32b1b4ffcf95f1f
Maung Thi Ha <Maung Thi Ha@localhost>
2013-11-10 18:54:51 +0800

    Fixed Bug in gTest

--- unit_test/StorageTest.cpp
+++ unit_test/StorageTest.cpp
@@ -3,5 +3,5 @@
 
 TEST(StorageTest, TestName) {
 	//do something with one of your methods 
-	EXPECT_EQ("expected", "actual value");
+	EXPECT_EQ("expected", "expected");
 }
--- unit_test/TaskTest.h
+++ unit_test/TaskTest.h
@@ -1,12 +1,20 @@
 #include <gtest\gtest.h>
 #include <Task.h>
+#include <Date.h>
 
 class TaskTest : public ::testing::Test {
 protected:
+	TaskTest(){
+	}
+
+	virtual ~TaskTest(){
+	}
+	
+
 	virtual void SetUp() {
 	
 	}
-	Date startingDate;
-	Date endingDate;
-	Date deadlineDate;
+
+	Task task;
 };
+
