b3c060a1e047b0f4e5ca4104c6a19b3826a8b142
lim_yi_min <lim_yi_min@hotmail.com>
2013-11-10 19:56:43 +0800

    Edited FormatFloatingTaskString in TaskTest

---
+++ unit_test/TaskLinkedListTest.cpp
@@ -0,0 +1,7 @@
+#include <gtest\gtest.h>
+#include "TaskLinkedListTest.h"
+
+TEST(TaskLinkedListTest, TestName) {
+	//do something with one of your methods 
+	EXPECT_EQ("expected", "expected");
+}
---
+++ unit_test/TaskTest.cpp
@@ -0,0 +1,20 @@
+#include "TaskTest.h"
+
+
+TEST_F(TaskTest, FormatFloatingTaskStringWithLocation) {
+	std::string action = "action";
+	std::string location = "location";
+	Date startingDate;
+	int startingTime = -1;
+	Date endingDate;
+	int endingTime = -1;
+	Date deadlineDate;
+	int deadlineTime = -1;
+	bool block = true;
+	
+	Task task(action, location, startingDate, startingTime, endingDate, endingTime, deadlineDate, deadlineTime, block);
+	std::string expectedTaskString = "   action at location";
+
+	EXPECT_EQ(expectedTaskString, task.formatTaskOutputString());
+}
+
